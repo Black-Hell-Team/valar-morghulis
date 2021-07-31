3cd0aeed8666f46fb031bbcf3780bb8788a7c9c7
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2021-07-31 12:18:04 -0300

    Fixing #1

---
+++ src/valar/utilities/antianalysis/ports.h
@@ -0,0 +1,40 @@
+#ifndef _PORTS_H_
+# define PORTS
+#include <windows.h>
+
+VOID 
+QuemuProcess(){
+    const TCHAR* qProcess[]{
+        TEXT("qemu-ga.exe"),
+    };
+}
+
+VOID
+VMWareProcess(){
+    const TCHAR* vProcess[]{
+        TEXT("vmtoolsd.exe"),
+        TEXT("vmwaretray.exe"),
+        TEXT("vmwareuser.exe"),
+        TEXT("VGAuthService.exe"),
+        TEXT("vmacthlp.exe"),
+    };
+}
+
+VOID
+VBoxProcess(){
+    const TCHAR* vbProcess[]{
+        TEXT("vboxservice.exe"),
+        TEXT("vboxtray.exe"),
+    };
+}
+
+VOID 
+VMWareMac(){
+    CHAR *BlockListMac[] = {
+        "00:50:56",
+        "00:1C:14",
+        "00:0C:29",
+        "00:05:69",
+    };
+}
+#endif /* PORTS */
