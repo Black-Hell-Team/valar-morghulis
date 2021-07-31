efbf31c7abc726943871cb46dd1b040955867de6
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2021-07-31 16:03:12 -0400

    declaring the first functions

--- src/valar/utilities/injector/Loader.h
+++ src/valar/utilities/injector/Loader.h
@@ -1,1 +1,17 @@
+#ifndef _LOADER_H_
+#define LOADER 
 
+// LDR ENTRY 
+
+enum {
+    khash32dll = 0x6A4ABC5B,
+    ndllhash = 0x3CFA685D,
+    vallochash = 0x91AFCA54
+};
+
+VOID DLLbase;
+VOID EntryPoint;
+typedef unsigned long SizeOfImage;
+typedef unsigned long Flags;
+
+#endif /* LOADER */
