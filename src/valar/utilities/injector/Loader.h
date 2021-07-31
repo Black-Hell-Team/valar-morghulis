d9c449231a66799e3c09adc63a02077977225f99
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2021-07-31 17:07:37 -0400

    add get process id parser

--- src/valar/utilities/injector/Loader.h
+++ src/valar/utilities/injector/Loader.h
@@ -1,9 +1,11 @@
 #ifndef _LOADER_H_
 #define LOADER 
 
-// LDR ENTRY 
+// Get process id 
+DWORD GprocessId(const std::tstring& Name);
 
-enum {
+// LDR ENTRY
+enum LDR_ENTRY {
     khash32dll = 0x6A4ABC5B,
     ndllhash = 0x3CFA685D,
     vallochash = 0x91AFCA54
